5a0e4ccbb530d02e2b99e2499e72e69cab8d480c
udon <naoki_udon@outlook.jp>
2018-04-28 00:14:39 +0900

    switch to melody

--- domain/service/websocket/websocket.go
+++ domain/service/websocket/websocket.go
@@ -3,17 +3,23 @@ package websocket
 import (
 	"net/http"
 
-	"github.com/trevex/golem"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
 )
 
 func Routing() {
-	router := golem.NewRouter()
-	router.On("connect", websocketHandle)
+	router := gin.Default()
+	m := melody.New()
 
-	http.HandleFunc("/connect", router.Handler())
-	http.ListenAndServe(":8080", nil)
+	group := router.Group("ws")
+	group.GET("/", func(g *gin.Context) {
+		m.HandleRequest(g.Writer, g.Request)
+	})
+	m.HandleConnect(websocketHandle)
+
+	http.ListenAndServe(":8080", router)
 }
 
-func websocketHandle(conn *golem.Connection) {
+func websocketHandle(conn *melody.Session) {
 
 }
