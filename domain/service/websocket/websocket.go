690e269f3716c3e7eaabccb30e68e676b32b2521
udon <naoki_udon@outlook.jp>
2018-05-12 02:42:28 +0900

    send message for websocket test

--- domain/service/websocket/websocket.go
+++ domain/service/websocket/websocket.go
@@ -1,6 +1,8 @@
 package websocket
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
@@ -13,6 +15,11 @@ func Routing(r *gin.Engine) {
 		m.HandleRequest(g.Writer, g.Request)
 	})
 	m.HandleConnect(websocketHandle)
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		log.Println(msg)
+		m.Broadcast(msg)
+	})
 }
 
 func websocketHandle(conn *melody.Session) {
